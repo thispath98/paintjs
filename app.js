0eca1ec3e7e55adcabeb82aafb625887095cc99f
thispath98 <lkr981147@gmail.com>
2021-01-12 19:23:55 +0900

    2.2 Recap

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext('2d');
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
